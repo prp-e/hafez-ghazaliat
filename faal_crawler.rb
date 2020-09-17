eb5a08e97c0d492c567a40843ee9f7318c806529
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-09-18 02:48:12 +0430

    written most of the parts

--- faal_crawler.rb
+++ faal_crawler.rb
@@ -1,9 +1,36 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("http://hafez-fal.ir/ghazal/001"))
-events_handler = doc.css("div.int-body")
+faal = [] 
 
-events_handler.each do |e| 
-    puts e.css("p")
+for ghazal_id in 0..494 do 
+    ghazal_id = ghazal_id + 1 
+    if ghazal_id < 10
+        doc = Nokogiri::HTML(open("http://hafez-fal.ir/ghazal/00#{ghazal_id}"))
+        events_handler = doc.css("div.int-body")
+        
+        events_handler.each do |e|
+            faal << e.css("p")
+            puts "Working on #{ghazal_id}"
+        end
+    elsif ghazal_id < 100
+        doc = Nokogiri::HTML(open("http://hafez-fal.ir/ghazal/0#{ghazal_id}"))
+        events_handler = doc.css("div.int-body")
+        
+        events_handler.each do |e|
+            faal << e.css("p")
+            puts "Working on #{ghazal_id}"
+        end 
+    else 
+        doc = Nokogiri::HTML(open("http://hafez-fal.ir/ghazal/#{ghazal_id}"))
+        events_handler = doc.css("div.int-body")
+        
+        events_handler.each do |e|
+            faal << e.css("p")
+            puts "Working on #{ghazal_id}"
+        end
+    end
+    sleep 0.25
 end 
+
+puts faal
