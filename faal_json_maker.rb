b3371ebab16aec77e8bba86c79684ebe3a28d466
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-09-18 14:54:08 +0430

    Now it works

--- faal_json_maker.rb
+++ faal_json_maker.rb
@@ -3,6 +3,7 @@ require 'json'
 faal_file = File.open("faal.txt", "r")
 
 faal = []
+faal_json = {}
 
 for line in faal_file.readlines do 
     faal << line 
@@ -10,5 +11,10 @@ end
 
 for item in 0..494 do 
     item = item +1 
-    puts "#{item} : #{faal[item - 1]}"
+    faal_json[item.to_s] = faal[item - 1]
 end 
+
+faal_json = faal_json.to_json 
+
+results = File.open("faal.json", "w")
+results.write(faal_json)
